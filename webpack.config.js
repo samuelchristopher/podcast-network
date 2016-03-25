c67bfadb8a3cb3e98047d8768a75a494e4c2a671
Samuel Christopher <thesamchris@gmail.com>
2016-03-25 07:33:36 +0000

    Created express server along with webpack integration

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: "./js/app.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: "babel-loader",
+        query: { presets: ['react', 'es2015'] }
+      }
+    ]
+  }
+}
