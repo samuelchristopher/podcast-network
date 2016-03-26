40cdafa322ec7a9c26c59b9106155e6eb9602a2f
Samuel Christopher <thesamchris@gmail.com>
2016-03-26 08:09:31 +0000

    Implemented podcastConnection with variables

--- js/components/Main.js
+++ js/components/Main.js
@@ -3,16 +3,26 @@ import Relay from 'react-relay';
 import Podcast from './Podcast';
 
 class Main extends React.Component {
+  // setLimit(e) {
+  //   let limit = Number(e.target.value);
+  //   this.props.relay.setVariables({
+  //     limit
+  //   });
+  // }
   render() {
-    const { podcasts: podcastsArray } = this.props.store;
-    const podcasts = podcastsArray.map((podcast) => {
+    const { edges: podcastsArray } = this.props.store.podcastConnection;
+    const podcasts = podcastsArray.reverse().map((edge) => {
       return (
-        <Podcast key={podcast._id} podcast={podcast} />
+        <Podcast key={edge.node.id} podcast={edge.node} />
       )
     });
     return (
       <div>
         <h1>Podcasts</h1>
+        // <select onChange={this.setLimit.bind(this)}>
+        //   <option value="5">5</option>
+        //   <option value="10" selected>10</option>
+        // </select>
         <ul>
           {podcasts}
         </ul>
@@ -22,12 +32,19 @@ class Main extends React.Component {
 }
 
 Main = Relay.createContainer(Main, {
+  initialVariables: {
+    limit: 6
+  },
   fragments: {
     store: () => Relay.QL`
       fragment on Store {
-        podcasts {
-          _id,
-          ${Podcast.getFragment('podcast')}
+        podcastConnection(last: $limit) {
+          edges {
+            node {
+              id,
+              ${Podcast.getFragment('podcast')}
+            }
+          }
         }
       }
     `
