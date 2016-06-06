dff3ca0c09c4b12bb5d7b6972461d14d1bb27ee9
Samuel Christopher <thesamchris@gmail.com>
2016-06-06 23:35:45 +0800

    Added custom download podcast button

--- js/components/podcast/PodcastSingle.js
+++ js/components/podcast/PodcastSingle.js
@@ -4,6 +4,7 @@ import Header from '../home/Header'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import CircularProgress from 'material-ui/CircularProgress'
+import RaisedButton from 'material-ui/RaisedButton'
 
 export default class PodcastSingle extends React.Component {
   constructor() {
@@ -31,10 +32,24 @@ export default class PodcastSingle extends React.Component {
     if (this.state.loading) {
       thingToShow = <CircularProgress size={1.5} color="#df405a" />
     } else {
+      const style = {
+        margin: '25px auto',
+        width: '100%'
+      }
+
       thingToShow = (
         <div>
           <h2 className="podcastSingle__title">{ podcast.title }</h2>
           <audio src={ podcast.url } controls></audio>
+          <br />
+          <RaisedButton
+            label="Download podcast"
+            linkButton={true}
+            href={podcast.url}
+            secondary={true}
+            download={true}
+            style={style}
+          />
         </div>
       )
     }
