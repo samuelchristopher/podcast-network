72474f2c1b5a1330afba325c2b4176fe02c60d31
Samuel Christopher <thesamchris@gmail.com>
2016-06-06 23:16:16 +0800

    Added material circular progress for loading

--- js/components/Main.js
+++ js/components/Main.js
@@ -3,6 +3,9 @@ import Podcast from './podcast/Podcast';
 import AddPodcast from './podcast/Add';
 import Header from './home/Header';
 import request from 'superagent';
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import CircularProgress from 'material-ui/CircularProgress'
 
 class Main extends React.Component {
 
@@ -31,17 +34,18 @@ class Main extends React.Component {
       return <Podcast key={podcast._id} podcast={podcast} />
     })
 
-    // TODO: Add Circular progress from Material-ui
     return (
-      <div>
-        <Header />
-        <div className="latest-podcasts">
-          <p className="latest-podcasts__label">Latest podcasts</p>
-          <div className="podcasts">
-            { this.state.loading ? <h2>Loading</h2> : podcasts }
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <div>
+          <Header />
+            <div className="latest-podcasts">
+              <p className="latest-podcasts__label">Latest podcasts</p>
+              <div className="podcasts">
+                { this.state.loading ? <CircularProgress size={1.5} color="#df405a" /> : podcasts }
+              </div>
           </div>
         </div>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
--- js/components/podcast/PodcastSingle.js
+++ js/components/podcast/PodcastSingle.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import request from 'superagent'
 import Header from '../home/Header'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import CircularProgress from 'material-ui/CircularProgress'
 
 export default class PodcastSingle extends React.Component {
   constructor() {
@@ -23,17 +26,30 @@ export default class PodcastSingle extends React.Component {
   }
   render () {
     const { podcast } = this.state
+    let thingToShow = ''
+
+    if (this.state.loading) {
+      thingToShow = <CircularProgress size={1.5} color="#df405a" />
+    } else {
+      thingToShow = (
+        <div>
+          <h2 className="podcastSingle__title">{ podcast.title }</h2>
+          <audio src={ podcast.url } controls></audio>
+        </div>
+      )
+    }
     return (
-      <div>
-        <Header />
-        <div className="latest-podcasts">
-          <p className="latest-podcasts__label">By { podcast.author } on { podcast.date }</p>
-          <div className="podcasts">
-            <h2 className="podcastSingle__title">{ podcast.title }</h2>
-            <audio src={ podcast.url } controls></audio>
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <div>
+          <Header />
+          <div className="latest-podcasts">
+            <p className="latest-podcasts__label">By { podcast.author } on { podcast.date }</p>
+            <div className="podcasts">
+              { thingToShow }
+            </div>
           </div>
         </div>
-      </div>
+      </MuiThemeProvider>
     )
   }
 }
